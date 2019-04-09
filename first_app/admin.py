1842b1464b9df6fe0c4ebf47b1116a52709f1896
manish826 <Manish.Kumar@iiitb.org>
2019-04-09 11:11:10 +0530

    spe

--- first_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from first_app.models import Product_Details
-
-# Register your models here.
-
-admin.site.register(Product_Details)
--- first_app/forms.py
+++
@@ -1,7 +0,0 @@
-from first_app.models import Product_Details
-from django import forms
-
-class ProductForm(forms.ModelForm):
-    class Meta():
-        model=Product_Details
-        fields=("Product_Name","Category","Description","Price","Negotiation","Picture")
